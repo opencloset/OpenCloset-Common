eaff50c6309a0414fe106a49d34d47b99e47327e
Hyungsuk Hong <aanoaa@gmail.com>
2016-05-02 18:51:03 +0900

    Add Constants Category and Measurement

---
+++ lib/OpenCloset/Common.pm
@@ -0,0 +1,25 @@
+package OpenCloset::Common;
+
+=encoding utf8
+
+=head1 NAME
+
+OpenCloset::Common - Common perl libs of OpenCloset
+
+=head1 Libs
+
+=head2 Constants
+
+=head3 OpenCloset::Constants::Category
+
+=head3 OpenCloset::Constants::Measurement
+
+=head1 COPYRIGHT
+
+The MIT License (MIT)
+
+Copyright (c) 2016 열린옷장
+
+=cut
+
+1;
