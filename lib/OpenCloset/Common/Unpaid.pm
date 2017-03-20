de1c3b72d332e2835e9d32b6161bf6451d006fe1
Hyungsuk Hong <aanoaa@gmail.com>
2017-03-20 12:57:42 +0900

    Add lib/OpenCloset/Common/Unpaid.pm

---
+++ lib/OpenCloset/Common/Unpaid.pm
@@ -0,0 +1,41 @@
+package OpenCloset::Common::Unpaid;
+
+require Exporter;
+@ISA       = qw/Exporter/;
+@EXPORT_OK = qw/unpaid2nonpaid unpaid2fullpaid nonpaid2fullpaid/;
+
+=encoding utf8
+
+=head1 NAME
+
+OpenCloset::Common::Unpaid - 미납금, 불납금 주문서의 상태변경
+
+=head1 METHODS
+
+=head2 unpaid2nonpaid
+
+=cut
+
+sub unpaid2nonpaid { }
+
+=head2 unpaid2fullpaid
+
+=cut
+
+sub unpaid2fullpaid { }
+
+=head2 nonpaid2fullpaid
+
+=cut
+
+sub nonpaid2fullpaid { }
+
+=head1 COPYRIGHT
+
+The MIT License (MIT)
+
+Copyright (c) 2016 열린옷장
+
+=cut
+
+1;
